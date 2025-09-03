9041ee9a515781a7cdd40ed366b70e8207be344e
YuliiaZas <julia.kv101@gmail.com>
2025-09-03 10:18:04 -0400

    feat: add items number validation

--- src/app/shared/validation/custom-validators.ts
+++ src/app/shared/validation/custom-validators.ts
@@ -1,5 +1,7 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
+type ConditionalObject = Record<string, number>;
+
 export const CustomValidators = {
   uniqueWithinArray:
     (array: string[]) =>
@@ -7,6 +9,18 @@ export const CustomValidators = {
       const value = formControl.value as string;
       if (array.includes(value)) {
         return { notUnique: true };
+      }
+      return null;
+    },
+  maxLengthConditional:
+    (conditionalObject: ConditionalObject, key: keyof ConditionalObject) =>
+    (formControl: AbstractControl): ValidationErrors | null => {
+      const value = formControl.value as string[];
+      if (!(key in conditionalObject)) {
+        return null;
+      }
+      if (value.length > conditionalObject[key]) {
+        return { maxLengthConditional: { requiredLength: conditionalObject[key], actualLength: value.length, key } };
       }
       return null;
     },
